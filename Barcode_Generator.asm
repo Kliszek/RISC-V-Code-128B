bb324b6779e3004e8bd12483431d5685fd028a5e
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-01-12 23:19:48 +0100

    Changed the project to support custom file sizes

--- Barcode_Generator.asm
+++ Barcode_Generator.asm
@@ -4,17 +4,17 @@
 #description : Generate BMP file with Code 128B barcode of a given word 
 #-------------------------------------------------------------------------------
 #
-#only 24-bits 600x50 pixels BMP files are supported
-.eqv BMP_FILE_SIZE 90054	#row width = 1800	image height = 50	pixel array size = 90000	DIB header = 40		header = 14	file size = 90054
-.eqv BYTES_PER_ROW 1800
+#24-bits 50 pixels high BMP files are supported
+.eqv MAX_FILE_SIZE 204854	#4096*50+54
+.eqv MAX_IMAGE_WIDTH 4096
 .eqv START_B 104
 .eqv STOP 106
 
 	.data
-#space for the 600x50px 24-bits bmp image
+#space for the 4096x50px 24-bits bmp image
 .align 4
 res:	.space 2
-image:	.space BMP_FILE_SIZE
+image:	.space MAX_FILE_SIZE
 i_name:	.asciz "output.bmp"
 
 mes1:	.asciz "Please specify the width of the narrowest bar (in pixels):\n"
@@ -99,23 +99,39 @@ quit_analyzing_input:
 	mul s2, s2, t0
 	addi s2, s2, 55		#10+11+length+11+13+10 (quiet zone + start symbol + the string + checksum + stop symbol + quiet zone)
 	mul s2, s2, s0		#multiplying by the width of the narrowest bar
-	li t0, 600
+				#s2 contains the pixel width of the image
+	li t0, MAX_IMAGE_WIDTH
 	la a2, l_err
-	bgt s2, t0, error	#error handling (if the word is too big for 600px bitmap)
+	bgt s2, t0, error	#error handling (if the word is too big for bitmap limit)
+	
+	li t0, 3
+	mul s1, s2, t0		#for now s1 contains number of bytes in row (excluding padding)
 	
 	
+	addi s1, s1, 3
+	srli s1, s1, 2
+	slli s3, s1, 2		#s3 contains row size with padding
+		
+	
+	li t0, 50
+	mul s1, s3, t0		#s1 contains pixel array size
+	addi t0, s1, 54
+
+#To sum up:	(everything will be used later)
+#	s0 - narrowest bar width
+#	s1 - pixel array size
+#	s2 - pixel width of the image
+#	s3 - row size in bytes
 	
 	li a7, 4
 	la a0, mes3
 	ecall			#Prints "Generating...:", so the user knows that he doesn't have to type anything anymore	
 	
-	
-	
 	#GENERATE BMP HEADER
 	la a0, image
 	li a1, 0x4D42		# "BM"
 	sh a1, (a0)
-	li a1, BMP_FILE_SIZE	#file size
+	addi a1, s1, 54		#file size	(pixel array size + (54 = header))
 	sw a1, 2(a0)
 				#reserved
 	sw zero, 6(a0)
@@ -127,8 +143,8 @@ quit_analyzing_input:
 	addi a0, a0, 14
 	li a1, 40		#DIB Header size
 	sw a1, (a0)
-	li a1, 600		#width
-	sw a1, 4(a0)
+				#width
+	sw s2, 4(a0)
 	li a1, 50		#height
 	sw a1, 8(a0)
 	li a1, 1		#number of planes
@@ -137,8 +153,8 @@ quit_analyzing_input:
 	sh a1, 14(a0)
 				#compression
 	sw zero, 16(a0)
-	li a1, 90000		#size of pixel array
-	sw a1, 20(a0)
+				#size of pixel array
+	sw s1, 20(a0)
 	li a1, 2835		#DPI (width)
 	sw a1, 24(a0)
 				#DPI (height)
@@ -155,7 +171,7 @@ quit_analyzing_input:
 	addi a0, a0, 54
 	li a1, 0xffffffff
 	
-	li t0, 90000
+	mv t0, s1
 	add t0, t0, a0
 paint_white:
 	sw a1, (a0)		#painting 4 bytes at once to speed up the process
@@ -170,44 +186,48 @@ quit_painting_white:
 	li t0, 10
 	mul a1, s0, t0		#calculating where to start: starting after the quiet zone
 	mv a2, s0
+	mv a3, s3
 	jal paint_char
 	
 	
-	la s1, input
-	li s2, 1		#index of current character
-	li s3, START_B		#the checksum	(starting from START B code)
+	la s4, input
+	li s5, 1		#index of current character
+	li s6, START_B		#THE CHECKSUM INITIAL VALUE	(starting from START B code)
 	
 read_input_loop:
-	lb a0, 1(s1)
+	lb a0, 1(s4)
 	beqz a0, quit_reading_input	#checking if the next character is 0 (skipping LINEFEED character)
 	
-	lb a0, (s1)
+	lb a0, (s4)
 	addi a0, a0, -32	#a0 contains a code of the next pattern
 	
-	mul t0, a0, s2
-	add s3, s3, t0		#increasing the checksum
+	mul t0, a0, s5
+	add s6, s6, t0		#increasing the checksum
 	
 	mv a2, s0
-	
+	mv a3, s3
 	
 	jal paint_char
 	
-	addi s1, s1, 1
-	addi s2, s2, 1
+	addi s4, s4, 1
+	addi s5, s5, 1
 	j read_input_loop
 quit_reading_input:
 	
 	li t0, 103
-	rem s3, s3, t0		#CALCULATED CHECKSUM
+	rem s6, s6, t0		#CALCULATED CHECKSUM
 	
-	mv a0, s3
+	mv a0, s6
 	mv a2, s0
+	mv a3, s3
 	jal paint_char		#painting the pattern for the checksum
 	
 	li a0, STOP
 	mv a2, s0
+	mv a3, s3
 	jal paint_char
 	
+	addi a0, s1, 54
 	jal save_file		#Saving the file
 
 
@@ -255,23 +275,26 @@ paint_bar:
 #	paints a black bar at a given width on the bitmap
 #arguments:
 #	a0 - the width at which the white bar will be added
+#	a1 - image row size in bytes (with padding)
 #return value:
 #	none
 	addi sp, sp, -4
 	sw s0, 0(sp)
 	
-	la a1, image
-	addi a1, a1, 54
+	la a2, image
+	addi a2, a2, 54
 	
 	li s0, 3
 	mul a0, a0, s0
-	add a0, a1, a0		#the address of the first pixel is in a0 now
+	add a0, a2, a0		#a0 contains the address of the first pixel to paint
 
 		
-	li s0, 90000
+	li s0, 50
+	mul s0, s0, a1		#HEREEEEEEEEEEEEEE	23 200 / 464
 	add s0, a0, s0		#s0 is the highest pixel in the column, the loop goes one pixel lower (1800 bytes) in each iteration
 paint_loop:
-	addi s0, s0, -1800
+	sub s0, s0, a1		#STOP HERE
+	#addi s0, s0, -464
 	
 	sb zero, (s0)
 	sb zero, 1(s0)
@@ -291,22 +314,25 @@ paint_char:
 #	paints a bar pattern of a character on the bitmap at given location
 #arguments:
 #	a0 - the character code
-#	a1 - the width in pixels (from left) to start painting the code
+#	a1 - the offset in pixels (from left) to start painting the code
 #	a2 - the scaling factor
+#	a3 - image row width in bytes (with padding)
 #return value:
-#	a1 - the width in pixels (from left) where the next pattern can be drawn
+#	a1 - the offset in pixels (from left) where the next pattern can be drawn
 
-	addi sp, sp, -20
-	sw ra, 16(sp)
-	sw s0, 12(sp)
-	sw s1, 8(sp)
-	sw s2, 4(sp)
-	sw s3, 0(sp)
+	addi sp, sp, -24
+	sw ra, 20(sp)
+	sw s0, 16(sp)
+	sw s1, 12(sp)
+	sw s2, 8(sp)
+	sw s3, 4(sp)
+	sw s4, 0(sp)
 	
 	
 	
 	mv s0, a1				#s0 is the current pixel horizontal location where we paint
 	mv s3, a2				#s3 is the scaling factor
+	mv s4, a3				#s4 is the image row width
 	
 	jal get_barcode
 	
@@ -322,6 +348,7 @@ whole_bar:
 	beq s0, s2, quit_painting_whole_bar
 	
 	mv a0, s0
+	mv a1, s4
 	jal paint_bar
 	
 	addi s0, s0, 1
@@ -341,12 +368,13 @@ quit_encode_loop:
 	
 	mv a1, s0
 	
-	lw s3, 0(sp)
-	lw s2, 4(sp)
-	lw s1, 8(sp)
-	lw s0, 12(sp)
-	lw ra, 16(sp)
-	addi sp, sp, 20
+	lw s4, 0(sp)
+	lw s3, 4(sp)
+	lw s2, 8(sp)
+	lw s1, 12(sp)
+	lw s0, 16(sp)
+	lw ra, 20(sp)
+	addi sp, sp, 24
 	jr ra
 
 
@@ -355,19 +383,22 @@ save_file:
 #description:
 #	when a file buffer is ready, this function saves the bmp file
 #arguments:
-#	none
+#	a0 - image size
 #return value:
 #	none
 
-	addi sp, sp, -4
-	sw s0, 0(sp)
+	addi sp, sp, -8
+	sw s0, 4(sp)
+	sw s1, 0(sp)
+	
+	mv s0, a0		#preserve image size
 	
 	li a7, 1024
 	li a1, 1
 	la a0, i_name
 	ecall			#Opens the file for writing
 
-	mv s0, a0		#preserve the file decriptor
+	mv s1, a0		#preserve the file decriptor
 		
 
 	la a2, s_err
@@ -377,13 +408,14 @@ save_file:
 	
 	li a7, 64
 	la a1, image
-	li a2, BMP_FILE_SIZE
+	mv a2, s0
 	ecall			#Writes the file
 	
-	mv a0, s0		#restore the file decriptor
+	mv a0, s1		#restore the file decriptor
 	li a7, 57
 	ecall			#Closes the file
 	
-	lw s0, 0(sp)
-	addi sp, sp, 4
+	lw s1, 0(sp)
+	lw s0, 4(sp)
+	addi sp, sp, 8
 	jr ra
